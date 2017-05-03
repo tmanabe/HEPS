7852c5197108693e98af1c18ef1658693d31a985
Tomohiro Manabe <tmanabe@users.noreply.github.com>
2017-05-03 10:30:48 +0900

    fix test frame

--- test/test_.py
+++ test/test_.py
@@ -3,26 +3,30 @@
 
 
 from glob import glob
+from os import path
 import subprocess
 from subprocess import PIPE
-import unittest
+from unittest import TestCase
 
 
-class TestHEPS(unittest.TestCase):
+class TestHEPS(TestCase):
     pass
 
 
-for html_path in glob(r'*.html'):
+dir = path.dirname(path.abspath(__file__))
+for html_path in glob(path.join(dir, '*.html')):
     def f(html_path):
         def g(self):
-            cmd = ' '.join(['phantomjs',
-                            'test_.js',
-                            '../HEPS.user.js',
-                            html_path])
-            cp = subprocess.run(cmd, stdout=PIPE, stderr=PIPE)
+            cmd = [
+                'phantomjs',
+                path.join(dir, 'test_.js'),
+                path.join(dir, '..', 'HEPS.user.js'),
+                'file:///' + html_path
+            ]
+            cp = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
             self.assertEqual(b'', cp.stdout)
             self.assertEqual(b'', cp.stderr)
         return g
     setattr(TestHEPS,
-            'test_%s' % html_path.rsplit('.', 1)[0],
+            'test_%s' % html_path.rsplit(path.sep, 1)[-1].rsplit('.', 1)[0],
             f(html_path))
